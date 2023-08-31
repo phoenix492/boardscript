565b4c4c6218738cd0d0b1f59c49e213995172da
pieman492 <rauljrrauljr@gmail.com>
2023-08-31 18:42:40 -0500

    Stuff i never commited

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "transformer.hpp"
 
-int main() {
-	
+int main(int argc, char* argv[]) {	
+	int value = 0;
+	MoveAgent* agent = new MoveAgent(30);
+	std::cin >> value;
+	std::cout << "Start value: " << value << std::endl;
+	std::cout << "Transformed: " << agent->transform(value) << std::endl; 
 }
